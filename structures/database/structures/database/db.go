7495af6ed71fab0db52bebfe19b18dbf2b1a3fa5
Nikita <71276784+niciki@users.noreply.github.com>
2022-07-29 12:51:59 +0300

    Add files via upload

---
+++ structures/database/structures/database/db.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	ls "github.com/niciki/go-NatsService/structures/localStore"
+	so "github.com/niciki/go-NatsService/structures/structOrder"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DatabaseRecord struct {
+	OrderUid  string `gorm:"primaryKey;"`
+	OrderJson string
+}
+
+type Database struct {
+	db *gorm.DB
+}
+
+func InitDb(port string) (Database, error) {
+	db, err := gorm.Open(postgres.Open(fmt.Sprintf("host=localhost port=%s user=user password=userwb dbname=db_wb sslmode=disable", port)))
+	if err != nil {
+		return Database{}, err
+	}
+	err = db.AutoMigrate(&DatabaseRecord{})
+	if err != nil {
+		return Database{}, err
+	}
+	return Database{db}, nil
+}
+
+func (d *Database) UploadCache(cache ls.Store) error {
+	var databaseData []DatabaseRecord
+	err := d.db.Find(&databaseData)
+	for _, rec := range databaseData {
+		var JsonOrder so.Order
+		json.Unmarshal([]byte(rec.OrderJson), &JsonOrder)
+		cache.Add(JsonOrder)
+	}
+	return err.Error
+}
+
+func (d *Database) AddRecord(rec ...so.Order) error {
+	log.Printf("Add to db record: %v", rec)
+	for _, r := range rec {
+		err := d.db.Create(DatabaseRecord{r.OrderUid, fmt.Sprint(r)})
+		if err.Error != nil {
+			return err.Error
+		}
+	}
+	return nil
+}
---
+++ structures/database/structures/structOrder/go.mod
@@ -0,0 +1,16 @@
+module github.com/niciki/go-NatsService/structures/structOrder
+
+go 1.18
+
+require (
+	github.com/e154/vydumschik v0.0.0-20151129100425-d3e462240df8
+	github.com/essentialkaos/translit v2.0.3+incompatible
+)
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	pkg.re/essentialkaos/check.v1 v1.2.0 // indirect
+)
